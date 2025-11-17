cd198dc8b71feeecd824c842fd64b5a7b07f488d
Tonchikxx <ustikantonina@gmail.com>
2025-11-18 00:56:32 +0300

    Создание проекта и Navbar

---
+++ vue-sneakers/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavMenu from './components/NavMenu.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <NavMenu />
+  </div>
+</template>
+
+<style scoped></style>
